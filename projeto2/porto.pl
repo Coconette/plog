959cef92f07c09c548f3c8b5d5ba1037855fe07f
Rneves3 <ricardo5a23@gmail.com>
2016-12-22 11:05:17 +0000

    started

--- projeto2/porto.pl
+++ projeto2/porto.pl
@@ -1,7 +1,9 @@
 :- use_module(library(random)).
 
+
 porto:-
-	initializePort(5, Port),
+	read(Size),
+	initializePort(Size, Port),
 	write(Port).
 	
 initializePort(0, []).
@@ -9,3 +11,30 @@ initializePort(N, [[]|T]):-
 	N > 0,
 	N1 is N - 1,
 	initializePort(N1, T).
+	
+
+	
+arriveShip(Ship):- 
+	disposeContainers(Port, Ship),
+	write('Ship is parking, handling containers'), nl,
+	write('Ship contains :'), nl,
+	displayShip(Ship).
+	
+	
+createShip([]).
+createShip([H|T]):-
+	random(0,5, SizeS),
+	Counter is SizeS - 1, 
+	random(0,8, Container),
+	(
+		Container = '0' -> H = 's',
+		Container = '1' -> H = 'm',
+		Container = '2' -> H = 'b',
+	)
+	createShip(T).
+	
+displayShip([]).
+displayShip([H|T]):-
+	write(H),
+	displayShip(T).
+	
